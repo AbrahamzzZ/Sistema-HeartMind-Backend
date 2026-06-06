94574182d66f79bd5066e60e8e90744f603e7c82
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-06-06 15:50:23 -0500

    refactor: Se reorganizó la clase Conexion para mejorar la legibilidad y estructura del código

--- api/config/conexion.php
+++ api/config/conexion.php
@@ -1,19 +1,25 @@
 <?php
 
-$host = getenv('DB_HOST');
-$dbname = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
+class Conexion
+{
+    public static function obtenerConexion(): PDO
+    {
+        $host = getenv('DB_HOST');
+        $dbname = getenv('DB_NAME');
+        $user = getenv('DB_USER');
+        $password = getenv('DB_PASSWORD');
 
-try {
-    $pdo = new PDO(
-        "mysql:host=$host;dbname=$dbname;charset=utf8",
-        $user,
-        $password
-    );
+        $pdo = new PDO(
+            "mysql:host=$host;dbname=$dbname;charset=utf8",
+            $user,
+            $password
+        );
 
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(
+            PDO::ATTR_ERRMODE,
+            PDO::ERRMODE_EXCEPTION
+        );
 
-} catch (PDOException $e) {
-    die($e->getMessage());
+        return $pdo;
+    }
 }
