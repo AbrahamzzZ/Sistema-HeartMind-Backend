30ec3d4ac4df6bfd23f007a0ea09dffd2a6c7e63
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-06-06 00:28:13 -0500

    feat: Se implementó el enrutamiento de la API para usuarios y evaluaciones, con manejo de rutas no encontradas

--- api/public/index.php
+++ api/public/index.php
@@ -1,6 +1,21 @@
 <?php
 
-echo json_encode([
-    "status" => "ok",
-    "message" => "API funcionando en Docker"
-]);
+$ruta = $_GET['ruta'] ?? '';
+
+switch ($ruta) {
+
+    case 'usuarios':
+        require_once __DIR__ . '/../routes/usuarios.php';
+        break;
+
+    case 'evaluaciones':
+        require_once __DIR__ . '/../routes/evaluaciones.php';
+        break;
+
+    default:
+        http_response_code(404);
+
+        echo json_encode([
+            'mensaje' => 'Ruta no encontrada'
+        ]);
+}
