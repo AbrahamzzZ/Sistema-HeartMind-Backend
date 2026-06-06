7397a3d7991dc9873b90e34051e38c20d6b4bce6
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-06-06 16:18:45 -0500

    feat: Se implementaron métodos de login y obtener perfil en UsuarioService y UsuarioController

--- api/controllers/usuarioController.php
+++ api/controllers/usuarioController.php
@@ -2,6 +2,8 @@
 
 class UsuarioController
 {
+    private const CONTENT_TYPE_JSON ='Content-Type: application/json';
+    private const FILE_GET_CONTENTS ='php://input';
     private UsuarioService $service;
 
     public function __construct(
@@ -12,17 +14,12 @@ class UsuarioController
 
     public function registrar(): void
     {
-        header('Content-Type: application/json');
+        header(self::CONTENT_TYPE_JSON);
 
         try {
 
-            $datos = json_decode(
-                file_get_contents('php://input'),
-                true
-            );
-
-            $resultado = $this->service
-                ->registrar($datos);
+            $datos = json_decode(file_get_contents(self::FILE_GET_CONTENTS), true);
+            $resultado = $this->service->registrar($datos);
 
             http_response_code(201);
 
@@ -38,6 +35,56 @@ class UsuarioController
             echo json_encode([
                 'success' => false,
                 'mensaje' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function login(): void
+    {
+        header(self::CONTENT_TYPE_JSON);
+
+        try {
+
+            $datos = json_decode(file_get_contents( self::FILE_GET_CONTENTS), true);
+            $resultado = $this->service->login($datos['correo'],$datos['contrasena']);
+
+            echo json_encode([
+                'success' => true,
+                'data' => $resultado
+            ]);
+
+        } catch (Exception $e) {
+
+            http_response_code(401);
+
+            echo json_encode([
+                'success' => false,
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function obtenerPerfil(): void
+    {
+        header(self::CONTENT_TYPE_JSON);
+
+        try {
+
+            $tokenData = AuthMiddleware::validarToken();
+            $usuario = $this->service->obtenerPerfil($tokenData->usuarioId);
+
+            echo json_encode([
+                'success' => true,
+                'data' => $usuario
+            ]);
+
+        } catch (Exception $e) {
+
+            http_response_code(401);
+
+            echo json_encode([
+                'success' => false,
+                'message' => $e->getMessage()
             ]);
         }
     }
