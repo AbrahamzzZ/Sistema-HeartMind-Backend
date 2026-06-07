62662d2098ffdf2f4eb3c610357443cfec938dc5
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-06-07 13:03:59 -0500

    feat: Agrega encabezados CORS y manejo de solicitudes OPTIONS en index.php

--- api/public/index.php
+++ api/public/index.php
@@ -2,6 +2,16 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: http://localhost:4200');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 $ruta = $_GET['ruta'] ?? '';
 
 switch ($ruta) {
